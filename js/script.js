24de1748e9a39985ba929e3c4be482f30d970d6b
Moh Bareche <32602327+MohBareche@users.noreply.github.com>
2023-08-24 05:55:31 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -153,13 +153,11 @@ const icon_Saillie = L.icon({
 })
 
 const greenIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+  iconUrl: '../img/green-circle-icon.svg',
   // shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
+  iconSize: [25, 25],
+  iconAnchor: [12, 12]
+  });
 
 const redIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
